81f3054b5c68cd25f2644f883ed673c6060ac68e
Vova <strikerxp1990@gmail.com>
2023-01-19 21:37:49 +0200

    First release

--- main.py
+++ main.py
@@ -4,26 +4,34 @@ from win10toast import ToastNotifier
 
 host = '8.8.8.8'
 toaster = ToastNotifier()
-notification_status = None
+notification_status = True
+checker = 0
 
 def internet_connection():
     request = os.system(f'ping -n 2 {host}')
     global notification_status
+    global checker
 
     if request == 0:
+        if checker == 1:
+            notification_status = True
+        checker = 0
         if notification_status == True:
-            toaster.show_toast('Internet ON', 'Internet is running NOW', threaded=True)
+            toaster.show_toast('Internet is ON', 'Internet is working', threaded=True)
+            notification_status = False
         print('Internet is ON')
-        notification_status = False
     if request == 1:
-        if notification_status == False:
+        if checker == 0:
+            notification_status = True
+        checker = 1
+        if notification_status == True:
             toaster.show_toast('Internet is OFF', 'Internet shuts DOWN', threaded=True)
+            notification_status = False
         print('Internet Shuts DOWN')
-        notification_status = True
 
 # Run once for 'notification_status' change
 internet_connection()
-time.sleep(4)
+time.sleep(2)
 os.system('cls')
 
 while True:
